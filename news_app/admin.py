174881ff21193dea83389fb534044c8da5d447ae
Mariya-Mukhitdinova <mukhitdinovamariya@gmail.com>
2024-11-15 17:17:28 +0500

    second

---
+++ news_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from news_app.models import CategoryNews, New
+# Register your models here.
+
+
+@admin.register(CategoryNews)
+class CategoryAdminModel(admin.ModelAdmin):
+    search_field = ["category_name"]
+    list_filter = ["created_at"]
+
+@admin.register(New)
+class NewAdminModel(admin.ModelAdmin):
+    search_fields = ["new_name"]
+    list_filter = ["created_at"]
---
+++ news_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class CategoryNews(models.Model):
+    category_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class New(models.Model):
+    new_name = models.CharField(max_length=55)
+    new_descr = models.TextField()
+    new_image = models.FileField(upload_to="new_image")
+    new_category = models.ForeignKey(CategoryNews, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.new_name, self.new_descr, self.new_image, self.created_at
+
+    class Meta:
+        verbose_name = "New"
+        verbose_name_plural = "News"
