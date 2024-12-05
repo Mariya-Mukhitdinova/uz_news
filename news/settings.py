39329a5611c96ce0be5796aca5cb534d831d7d53
Mariya-Mukhitdinova <mukhitdinovamariya@gmail.com>
2024-12-05 13:43:09 +0500

    26 DZ

--- news/settings.py
+++ news/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#Подключение БД PostgresSQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'uz_news',
+        'USER': 'postgres',
+        'POST': 5432,
+        'HOST': 'localhost',
+        'PASSWORD': 'mi10mariya'
     }
 }
 
